83c8c86aa2926ea3ae4e8feddd7afe7ef6aa0ff9
beingtechsavy <arpanpadhi2004@gmail.com>
2023-10-02 00:00:19 +0530

    day 3 changes

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
-app=Flask(__name__)
-@app.route("/")
-def Hello_world():
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=['GET'])
+def hello_world():
     return render_template("home.html")
-if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True)
+
+@app.route("/signup")
+def signup():
+    return render_template("register.html")
+
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    return render_template("project.html")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
