be2379c51dd25b8013b4df78ba0271ff2923db7d
beingtechsavy <arpanpadhi2004@gmail.com>
2023-10-17 22:04:01 +0530

    added dynamic data and API routes into the file.

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
-import psycopg2
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 
+Project=[{
+    'id':1,
+    'title':'healthcare website',
+    'College':'ITER',
+    'skills':'React js',
+    'salary':1500000
+},
+{
+    'id':2,
+    'title':'Arduino robot',
+    'College':'NIT',
+    'skills':'Machine Learning',
+    'salary':15000
+},
+{
+    'id':3,
+    'title':'fitness mobile app',
+    'College':'NIST',
+    'skills':'Java'
+    
+}]
 
-
-@app.route("/", methods=['GET'])
+@app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template('home.html',pro=Project)
 
-@app.route("/signup")
-def signup():
-    return render_template("register.html")
-
-@app.route('/submit', methods=['POST','GET'])
-def submit():
-    return render_template("project.html")
+@app.route("/api/projects")
+def list_projects():
+    return jsonify(Project)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
