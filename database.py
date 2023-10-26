159123c073c3e85a06da7bc570d8cd1c355a6c60
Arpan Padhi <arpanpadhi2004@gmail.com>
2023-10-26 12:18:16 +0530

    .

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine, text
 
 # Removed unnecessary import of "sqlalchemy"
 
-connection_string = "mysql+pymysql://8o8e7e67yr1nwv462obz:pscale_pw_NPwt077xvg80gEIAFv7oTspg5qkymTYuWK3jOFVHCcu@aws.connect.psdb.cloud:3306/skillskope"
+connection_string = "mysql+pymysql://h6y63ob127ts1e9nhizp:pscale_pw_WVYUqxAtVTNOwvygvB7v1f43RAcowesXXg9gVP1r0H4@aws.connect.psdb.cloud:3306/skillskope"
 # Update connection string with the actual database details
 
-engine = create_engine(connection_string, connect_args={"ssl":{"ssl_ca": "/etc/ssl/cert.pem"}},pool_pre_ping=True)
+engine = create_engine(connection_string, connect_args={"ssl":{"ssl_ca": "/etc/ssl/cert.pem"}},pool_pre_ping=True,pool_recycle=3600)
 print(sqlalchemy.__version__)
 
 
