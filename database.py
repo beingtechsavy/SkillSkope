94ba0d1e9bc6b95b594a3ec292a852098f564f88
Arpan Padhi <arpanpadhi2004@gmail.com>
2023-11-06 23:39:13 +0530

    some new changes

--- database.py
+++ database.py
@@ -1,19 +1,26 @@
-from select import select
-import sqlalchemy
+
+# from select import select
+# import sqlalchemy
 from sqlalchemy import create_engine, text
+import os
 
 
-
-connection_string = "mysql+pymysql://9pi97xuivkh875kmktfm:pscale_pw_nnjHhTqHuc996wkchVSjrnSnXw4MoAtbC8mIVWRsgEg@aws.connect.psdb.cloud:3306/skillskope"
+connection_string = os.getenv("c_string")
 
 
 engine = create_engine(connection_string, connect_args={"ssl":{"ssl_ca": "/etc/ssl/cert.pem"}})
-print(sqlalchemy.__version__)
+# print(sqlalchemy.__version__)
 def load_projects_from_db():
+    # with engine.connect() as conn:
+    #     result = conn.execute(text("select * from projects"))
+    #     projects = [row._mapping for row in result.all()]
+    #     return projects
     with engine.connect() as conn:
         result = conn.execute(text("select * from projects"))
-        projects = [row._mapping for row in result.all()]
-        return projects
 
+    result_dicts = []
+    for row in result.all():
+        result_dicts.append(dict(row._mapping))
+    return result_dicts
 
 
